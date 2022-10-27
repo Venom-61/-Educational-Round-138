23e41f81fd35fed5b1dca5827c3e2a711e592ec3
Venom-61 <subhendukarmakar61@gmail.com>
2022-10-27 12:58:18 +0530

    Death's Blessing

---
+++ b.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for(auto &x : a) cin >> x;
+    for(auto &x : b) cin >> x;
+    
+    sort(b.begin(), b.end());
+
+    long long ans = 0;
+    for(int i = 0; i < n; i++) {
+        ans += a[i];
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        ans += b[i];
+    }
+
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; ++i) {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+
+    return 0;
+}
