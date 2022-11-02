1fbaefd053b5f1655c776f128766a9245a10f12f
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-03 02:45:17 +0530

    Number Game

--- c.cpp
+++ c.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 #define ll long long
 
+// C. Number Game
+
 void solve() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+
+    int ans = 0;
+    for(int k = 1; k <= n; k++) {
+        multiset<int> s(a.begin(), a.end());
+        for(int i = 0; i < k; i++) {
+            auto it = s.lower_bound(k - i + 1);
+            s.erase(--it);
+            if()
+        }
+    }
 }
 
 int32_t main() {
